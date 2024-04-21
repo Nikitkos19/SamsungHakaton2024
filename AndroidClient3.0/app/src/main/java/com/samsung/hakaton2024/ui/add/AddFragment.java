f1996102076d528c55016f47adecedf6fb28669f
azzazichka <muhametov_max10@mail.ru>
2024-04-21 14:57:55 +0700

    alina & max

--- AndroidClient3.0/app/src/main/java/com/samsung/hakaton2024/ui/add/AddFragment.java
+++ AndroidClient3.0/app/src/main/java/com/samsung/hakaton2024/ui/add/AddFragment.java
@@ -1,4 +1,90 @@
 package com.samsung.hakaton2024.ui.add;
 
-public class AddFragment {
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.material.button.MaterialButton;
+import com.samsung.hakaton2024.R;
+import com.samsung.hakaton2024.databinding.FragmentAddBinding;
+
+import java.util.Objects;
+
+
+public class AddFragment extends Fragment {
+
+
+    private FragmentAddBinding binding;
+    MaterialButton btn;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        AddViewModel addViewModel =
+                new ViewModelProvider(this).get(AddViewModel.class);
+
+        binding = FragmentAddBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+  /*      PopupMenu popupMenu = new PopupMenu(requireContext(), view.findViewById(R.id.navigation_notifications));
+        popupMenu.inflate(R.menu.bottom_nav_menu);
+        Menu bottomNavMenu = popupMenu.getMenu();
+        MenuItem btn = bottomNavMenu.findItem(R.id.navigation_notifications);
+
+   */
+        //
+        binding.btnAddPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.with(AddFragment.this)
+                        .crop()                    //Crop image(Optional), Check Customization for more option
+                        .compress(1024)            //Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)    //Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+            }
+        });
+
+        binding.btnAddFrag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SecondAddFragment secondFrag = new SecondAddFragment();
+
+                FragmentTransaction ft = getChildFragmentManager().beginTransaction();
+                ft.replace(R.id.fragment_container, secondFrag).addToBackStack(null);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.commit();
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        assert data != null;
+        Uri uri = data.getData();
+        binding.imgAddFrag.setImageURI(uri);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
--- AndroidClient3.0/app/src/main/java/com/samsung/hakaton2024/ui/home/HomeFragment.java
+++ AndroidClient3.0/app/src/main/java/com/samsung/hakaton2024/ui/home/HomeFragment.java
@@ -9,17 +9,12 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
-import androidx.viewpager2.widget.ViewPager2;
 
-import com.samsung.hakaton2024.R;
 import com.samsung.hakaton2024.databinding.FragmentHomeBinding;
-import com.samsung.hakaton2024.scroll_covers.MyAdapter;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
-
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,13 +23,9 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        ViewPager2 pager = binding.pager;
 
-//        ViewPager2 pager = findViewById(R.id.pager);
-        FragmentStateAdapter pageAdapter = new MyAdapter(this);
-        pager.setAdapter(pageAdapter);
-//        final TextView textView = binding.textHome;
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
