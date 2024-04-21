1a7e47b559060764510e5e866d37b1f42cdade7d
azzazichka <muhametov_max10@mail.ru>
2024-04-21 15:22:48 +0700

    alina x max

--- AndroidClient3.0/app/src/main/java/com/samsung/hakaton2024/MainActivity.java
+++ AndroidClient3.0/app/src/main/java/com/samsung/hakaton2024/MainActivity.java
@@ -9,11 +9,8 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
-import androidx.viewpager2.widget.ViewPager2;
 
 import com.samsung.hakaton2024.databinding.ActivityMainBinding;
-import com.samsung.hakaton2024.scroll_covers.MyAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,13 +23,11 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
-
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_add, R.id.navigation_profile)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
--- AndroidClient3.0/app/src/main/java/com/samsung/hakaton2024/ui/home/HomeFragment.java
+++ AndroidClient3.0/app/src/main/java/com/samsung/hakaton2024/ui/home/HomeFragment.java
@@ -12,7 +12,6 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
-import com.samsung.hakaton2024.R;
 import com.samsung.hakaton2024.databinding.FragmentHomeBinding;
 import com.samsung.hakaton2024.scroll_covers.MyAdapter;
 
@@ -20,19 +19,23 @@ public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
+
+
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+
         ViewPager2 pager = binding.pager;
 
 //        ViewPager2 pager = findViewById(R.id.pager);
         FragmentStateAdapter pageAdapter = new MyAdapter(this);
         pager.setAdapter(pageAdapter);
+//
 //        final TextView textView = binding.textHome;
 //        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
